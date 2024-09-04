0b41daa018f7e0f4ad59394983bb44e7c0fa393a
nicksyzs <167319962+nicksyzs@users.noreply.github.com>
2024-09-04 09:16:15 -0300

    aula 04/09/2024

---
+++ aula 04092024/script.js
@@ -0,0 +1,8 @@
+alert(Math.PI);
+alert(Math.abs(5.9));
+alert(Math.max(5.9, 6, 12, -80));
+alert(Math.round(12.6));
+alert(Math.floor(45.5));
+alert(Math.ceil(45.5));
+alert(Math.pow(12, 3));
+alert(Math.sqrt(16));
